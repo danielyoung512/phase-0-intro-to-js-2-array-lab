4f9b6addb201067a1d47e1d344cbe7020b6d4a3f
Daniel Young <danielcyoung512@gmail.com>
2023-09-09 13:32:22 -0500

    array lab update

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+  // cats.push("Ralph");
+  // console.log(cats);
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+    console.log(cats);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+    console.log(cats);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop()
+    console.log(cats)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift()
+    console.log(cats)
+}
+
+function appendCat(name) {
+    const copyOfCats = cats.slice();
+    copyOfCats.push(name)
+    console.log(copyOfCats)
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCats = cats.slice();
+    copyOfCats.unshift(name)
+    console.log(copyOfCats)
+    return copyOfCats
+}
+
+function removeLastCat(name) {
+    const copyOfCats = cats.slice(0, 2)
+    console.log(copyOfCats)
+    return copyOfCats
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = cats.slice(1)
+    console.log(copyOfCats)
+    return copyOfCats
+}
